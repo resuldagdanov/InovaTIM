dba6258280b4e75e159613a596a02d1a30ba9a37
Resul Dagdanov <60505033+ResulDagdanov@users.noreply.github.com>
2020-01-31 13:21:31 +0300

    Create ultrasonik_sensor.ino
    İnovaTİM'in liseli öğrenciler için yaptığı Görüntü İşleme ve Yapay Zeka Eğitimi.
    
    Arduino ile ultrasonik sensör ile mesafe ölçümü uygulaması.

---
+++ Görüntü İşleme ve Yapay Zeka Eğitimi/ultrasonik_sensor.ino
@@ -0,0 +1,39 @@
+// dijital pinleri tanımlamak
+const int trigPin = 6;
+const int echoPin = 7;
+
+// değişkenlerin tanımlanması
+long sure;
+int mesafe;
+
+// setup fonksiyonunu başlat
+void setup()
+{
+  pinMode(trigPin, OUTPUT); // trigger pinini OUTPUT olarak tanımlamak
+  pinMode(echoPin, INPUT);  // echo pinini INPUT olarak tanımlamak
+  
+  Serial.begin(9600);       // 9600 Baud oranında Serial haberleşmeyi başlat
+}
+ 
+// loop fonksiyonunu başlat
+void loop()
+{
+  // başta trigger pinini temizlemek
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // trigger pinine 10 milisaniye boyunca güç ver ve sonra gücü kes
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // echo pinini okumak, çıkış olarak ses dalgasının yolculuk zamanını mikrosaniye cinsinden verir
+  sure = pulseIn(echoPin, HIGH);
+  
+  // mesafe hesaplamak
+  mesafe= sure*0.034/2;
+  
+  // Serial Monitora yazdırmak
+  Serial.print("Mesafe: ");
+  Serial.println(mesafe);
+}
