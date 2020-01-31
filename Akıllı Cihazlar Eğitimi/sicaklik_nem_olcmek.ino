e60c74ab1dbce4ed8fb20862dbbb9417165991b2
Resul Dagdanov <60505033+ResulDagdanov@users.noreply.github.com>
2020-01-31 17:08:57 +0300

    Create sicaklik_nem_olcmek.ino

---
+++ Akıllı Cihazlar Eğitimi/sicaklik_nem_olcmek.ino
@@ -0,0 +1,39 @@
+# turta sıcaklık ve nem sensörünün kütüphanesinin eklenmesi
+#include <Turta_TempRH_Module.h>
+
+// sıcaklık ve nem ölçümü için kütüphane objesini oluştur
+Turta_TempRH_Module th;
+
+// setup fonksiyonunun başlatılması
+void setup() 
+{
+  // sensör objesinin başlatılması
+  th.begin();
+
+  // serial portun başlatılması
+  Serial.begin(115200);
+  delay(200);
+}
+
+void loop()
+{
+  // sıcaklık verisinin sensörden okunması ve sicaklik değişkenine atanması
+  double sicaklik = th.readTemperature();
+  
+  // nem verisinin sensörden okunması ve nem değişkenine atanması
+  double nem = th.readHumidity();
+  
+  // elde edilen sicaklik verilerisini serial monitora yazdırmak
+  Serial.print("Sicaklik: ");
+  Serial.println(sicaklik);
+  
+  // elde edilen nem verilerisini serial monitora yazdırmak
+  Serial.print("Nem: ");
+  Serial.println(nem);
+
+  // serial monitora boş bir satırın enlenmesi
+  Serial.println("");
+
+  // 1000ms = 1 saniye beklemek
+  delay(1000);
+}
